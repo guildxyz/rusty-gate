20716c0761931732513bc617cea5459faf8c212a
richardanyalai <gtaricsi@gmail.com>
2022-10-18 13:53:35 +0200

    Added NoSuchChain type

--- src/requirements/errors.rs
+++ src/requirements/errors.rs
@@ -4,4 +4,6 @@ use thiserror::Error;
 pub enum CheckableError {
     #[error("Missing field `{0}`")]
     MissingField(String),
+    #[error("Chain `{0}` is not supported")]
+    NoSuchChain(String),
 }
