9e5e69c992c8b1aa52544f21fc0a740ce1eb7172
richardanyalai <gtaricsi@gmail.com>
2022-11-30 15:58:43 +0100

    Refactored ABIs

--- gate/src/requirements/general/token/mod.rs
+++ gate/src/requirements/general/token/mod.rs
@@ -4,4 +4,5 @@ mod nft;
 pub use erc20::Erc20Requirement;
 pub use nft::*;
 
-pub const ERC_ABI: &[u8] = include_bytes!("../../../../../abi/TOKEN.json");
+pub const ERC20_ABI: &[u8] = include_bytes!("../../../../../abi/ERC20.json");
+pub const ERC721_ABI: &[u8] = include_bytes!("../../../../../abi/ERC721.json");
