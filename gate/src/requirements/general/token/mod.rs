a0d0c90d03cd2b25c4df32cf5e72df11a569e122
richardanyalai <gtaricsi@gmail.com>
2022-11-30 17:42:55 +0100

    Added ERC1155 ABI

--- gate/src/requirements/general/token/mod.rs
+++ gate/src/requirements/general/token/mod.rs
@@ -6,3 +6,4 @@ pub use nft::*;
 
 pub const ERC20_ABI: &[u8] = include_bytes!("../../../../../abi/ERC20.json");
 pub const ERC721_ABI: &[u8] = include_bytes!("../../../../../abi/ERC721.json");
+pub const ERC1155_ABI: &[u8] = include_bytes!("../../../../../abi/ERC1155.json");
