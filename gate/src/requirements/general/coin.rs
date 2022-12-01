1d69c6dc5d2b87c95284d18188c9591639560e7a
richardanyalai <gtaricsi@gmail.com>
2022-12-01 10:59:09 +0100

    Fixed weird nested error

--- gate/src/requirements/general/coin.rs
+++ gate/src/requirements/general/coin.rs
@@ -85,9 +85,7 @@ impl TryFrom<&Requirement> for CoinRequirement {
         match req.chain {
             Some(chain) => {
                 if PROVIDERS.get(&(chain as u8)).is_none() {
-                    return Err(CheckableError::NoSuchChain(
-                        CheckableError::NoSuchChain(format!("{:?}", chain)).to_string(),
-                    ));
+                    return Err(CheckableError::NoSuchChain(format!("{:?}", chain)));
                 }
 
                 let res = CoinRequirement {
